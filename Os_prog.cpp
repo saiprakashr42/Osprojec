37fc4153d97747d971a132ee00ea157a38456f62
saiprakashr42 <45858006+saiprakashr42@users.noreply.github.com>
2020-03-25 12:25:51 +0530

    Create Os_prog.cpp

---
+++ Os_prog.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+void round_r(int num,int remt[20],int Curr_t[20],int bst[20]);
+
+main(){
+int Proc_num,
+int j,num,Curt,RemProc,indicator,time_quan,wait,tut,art[20],bst[20],remt[10],x=1;
+int indicator=0;
+int wait=0;
+int tut=0;
+printf("Enter the total number of processes: ");
+scanf("%d",&num);
+RemProc=num;
+
+printf("\nEnter the arrival time and Burst time of the processes\n");
+for(Proc_num=0;Proc_num<num;Proc_num++)
+ {
+    printf("\nProcess P%d\n",Proc_num+1);
+    printf("Arrival time =  "); 
+		scanf("%d",&art[Proc_num]);
+		printf("BurstTime = "); 
+		scanf("%d",&bst[Proc_num]); 
+		remt[Proc_numbst[Proc_num]; 
+	} 
+  
+  
+  //The time quantum for first is 6
+  //Time quantum for second is 10
+  
+  
+  
+  
+    
