582b38d408b43ef6fde807778ee10da84fbd7bed
saiprakashr42 <45858006+saiprakashr42@users.noreply.github.com>
2020-03-26 20:23:04 +0530

    Update Os_prog.cpp

--- Os_prog.cpp
+++ Os_prog.cpp
@@ -18,15 +18,53 @@ for(Proc_num=0;Proc_num<num;Proc_num++)
  {
     printf("\nProcess P%d\n",Proc_num+1);
     printf("Arrival time =  "); 
-		scanf("%d",&art[Proc_num]);
-		printf("BurstTime = "); 
-		scanf("%d",&bst[Proc_num]); 
-		remt[Proc_numbst[Proc_num]; 
-	} 
+    scanf("%d",&art[Proc_num]);
+    printf("BurstTime = "); 
+    scanf("%d",&bst[Proc_num]); 
+    remt[Proc_num]=bst[Proc_num]; 
+	
+} 
   
   
   //The time quantum for first is 6
   //Time quantum for second is 10
+time_quan=3;
+Curt=0;
+	
+for(Proc_num=0;RemProc!=0;){
+	
+	if(remt[Proc_num]<=time_quan && remt[Proc_no]>0)
+		{ 
+			Curt+=remt[Proc_num]; 
+			remt[Proc_num]=0; 
+			indicator=1; 
+		} 
+	else if(remt[Proc_num]>0)
+		{ 
+			remt[Proc_num]-=time_quan; 
+			Curt+=time_quan; 
+		}
+	
+	if(remt[Proc_num]==0 && indicator==1)			
+		{ 	
+			printf("%d",Proc_num);
+			RemProc--;				
+			printf("P %d",Proc_num+1); 
+			printf("\t\t\t%d",Curt-art[Proc_num]);
+			printf("\t\t\t%d\n",Curt-bst[Proc_num]-art[Proc_num]);
+			wait+=Curt-art[Proc_num]-bst[Proc_num]; 
+			tut+=Curt-art[Proc_num]; 
+			indicator=0; 
+                       
+		} 
+	
+	
+
+	
+		     
+		     
+		     
+		     
   
   
   
